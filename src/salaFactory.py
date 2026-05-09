97e074560a9fd22af663fef3d45fa607fe2a5cc7
Rafael Freire <rafaelfmg72@gmail.com>
2026-05-09 16:05:13 -0300

    Create factory for Sala

---
+++ src/salaFactory.py
@@ -0,0 +1,22 @@
+from sala import Sala
+from laboratorio import Laboratorio
+from salaAula import SalaDeAula
+from salaEstudos import SalaDeEstudos
+from enum import Enum
+
+class TipoDeSala(Enum):
+	SALA_ESTUDOS = 1
+	SALA_AULA = 2
+	LABORATORIO = 3
+
+class SalaFactory:
+	def get_instance(self, tipo_de_sala: int, local: str, capacidade: int, other: Any = None) -> Sala:
+		match TipoDeSala:
+			case TipoDeSala.SALA_ESTUDOS:
+				return SalaDeEstudos(local, capacidade, other)
+			case TipoDeSala.SALA_AULA:
+				return SalaDeAula(local, capacidade)
+			case TipoDeSala.LABORATORIO:
+				return Laboratorio(local, capacidade, other)
+			case _:
+				raise ValueError("Valor inválido.")
