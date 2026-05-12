74e72b1b2f1943005ed8cf531cce74d030eccaaf
Rafael Freire <rafaelfmg72@gmail.com>
2026-05-11 23:12:11 -0300

    Doc: usuario

--- src/professor.py
+++ src/professor.py
@@ -1,6 +1,12 @@
 from usuario import Usuario
 
 class Professor(Usuario):
+	"""
+	Subclasse de usuário que representa um docente.
+
+	Atributos:
+	- matricula: identificador único do docente.
+	"""
 	def __init__(self, name: str, matricula: int) -> None:
 		self.name = name
 		self.matricula = matricula
